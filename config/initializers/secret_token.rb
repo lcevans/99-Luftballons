1f741f6970156176eb1d43e0d6a77c026aee87d7
Chris Evans <lawrence.c.evans@gmail.com>
2013-10-12 13:03:15 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinetyNineLuftballons::Application.config.secret_token = '5d7905a4159dc3df740ee6311fe7a6d55e5caa1df811232e65f76673853072be2fd1a96b50b4719324d5a91ce7d108d5383b8759934f26d8ee15c2e793394d5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NinetyNineLuftballons::Application.config.session_store :cookie_store, key: '_ninety_nine_luftballons_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NinetyNineLuftballons::Application.config.session_store :active_record_store
